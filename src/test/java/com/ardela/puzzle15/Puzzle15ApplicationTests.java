eef051efd2db1e50fe2016da2f7909af88d1eceb
Billy Ardela <billy@propstep.com>
2025-04-23 21:42:39 +0200

    Created backend puzzle15 as a SpringBoot WebApp

---
+++ src/test/java/com/ardela/puzzle15/Puzzle15ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ardela.puzzle15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Puzzle15ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
