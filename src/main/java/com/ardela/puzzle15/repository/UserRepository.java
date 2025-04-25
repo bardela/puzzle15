bcee6920c5d80e910cd0681c3b51e03f81cf4b69
Billy Ardela <billy@propstep.com>
2025-04-25 18:30:28 +0200

    UserRepository and AuthenticatedUserService

---
+++ src/main/java/com/ardela/puzzle15/repository/UserRepository.java
@@ -0,0 +1,26 @@
+package com.ardela.puzzle15.repository;
+
+import com.ardela.puzzle15.model.User;
+import com.ardela.puzzle15.repository.base.InMemoryRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository extends InMemoryRepository<User> {
+
+  @Override
+  protected User copyData(User origin) {
+    User user = new User();
+    user.setUsername(origin.getUsername());
+    user.setPasswordHash(origin.getPasswordHash());
+    user.setRol(origin.getRol());
+    return user;
+  }
+
+  public User findByUserName(String username) {
+    return collection.values().stream()
+        .filter(user -> user.getUsername().equals(username))
+        .findFirst()
+        .orElse(null);
+  }
+
+}
---
+++ src/main/java/com/ardela/puzzle15/service/AuthenticatedUserService.java
@@ -0,0 +1,35 @@
+package com.ardela.puzzle15.service;
+
+import com.ardela.puzzle15.model.User;
+import com.ardela.puzzle15.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticatedUserService {
+
+  private final UserRepository userRepository;
+  public AuthenticatedUserService(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  public UserDetails findByUsername(String username) {
+    User user = userRepository.findByUserName(username);
+    if (user == null) {
+      throw new UsernameNotFoundException("Authenticated user not found");
+    }
+
+    return org.springframework.security.core.userdetails.User
+        .withUsername(user.getUsername())
+        .password(user.getPasswordHash())
+        .roles(user.getRol())
+        .build();
+  }
+
+  public boolean passwordsMatch(String password, String encodedPassword) {
+    BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+    return bCryptPasswordEncoder.matches(password, encodedPassword);
+  }
+}
