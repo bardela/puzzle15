3bbddebbde81d7c1c1b71f683355b6b9750705e6
Billy Ardela <billy@propstep.com>
2025-04-26 16:49:27 +0200

    Constants class

---
+++ src/main/java/com/ardela/puzzle15/utils/GameConstants.java
@@ -0,0 +1,7 @@
+package com.ardela.puzzle15.utils;
+
+public class GameConstants {
+  public static final int DIMENSION = 4;
+  public static final int MAX_PIECE = DIMENSION * DIMENSION - 1;
+  public static final int EMPTY_PIECE = 0;
+}
